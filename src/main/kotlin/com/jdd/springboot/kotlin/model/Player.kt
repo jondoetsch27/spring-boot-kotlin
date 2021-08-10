830c45e6131a13b002cf15150e4fac215b283f50
jondoetsch27 <46655911+jondoetsch27@users.noreply.github.com>
2021-08-10 09:51:51 -0700

    08/10 - model layer updates @jondoetsch27

--- src/main/kotlin/com/jdd/springboot/kotlin/model/Player.kt
+++ src/main/kotlin/com/jdd/springboot/kotlin/model/Player.kt
@@ -1,4 +1,26 @@
 package com.jdd.springboot.kotlin.model
 
-class Player {
-}
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Player constructor(
+
+    @JsonProperty("playerId")
+    val playerId: String,
+
+    @JsonProperty("playerFirstName")
+    val playerFirstName: String,
+
+    @JsonProperty("playerLastName")
+    val playerLastName: String,
+
+    @JsonProperty("playerNumber")
+    val playerNumber: String,
+
+    @JsonProperty("playerPosition")
+    val playerPosition: String,
+
+    @JsonProperty("playerTeam")
+    val playerTeam: String
+)

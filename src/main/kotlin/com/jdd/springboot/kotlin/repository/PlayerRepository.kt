e8ba6ed72e3c837d45495e8149aaa9dc7f26002b
jondoetsch27 <46655911+jondoetsch27@users.noreply.github.com>
2021-08-10 09:52:18 -0700

    08/10 - data layer updates @jondoetsch27

--- src/main/kotlin/com/jdd/springboot/kotlin/repository/PlayerRepository.kt
+++ src/main/kotlin/com/jdd/springboot/kotlin/repository/PlayerRepository.kt
@@ -2,6 +2,8 @@ package com.jdd.springboot.kotlin.repository
 
 import com.jdd.springboot.kotlin.model.Player
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PlayerRepository : CrudRepository<Player, String> {
 }

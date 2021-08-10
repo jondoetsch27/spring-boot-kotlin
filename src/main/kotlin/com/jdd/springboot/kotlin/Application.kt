6461189b532381a87b92d8f8786eb147c25449d7
jondoetsch27 <46655911+jondoetsch27@users.noreply.github.com>
2021-08-10 09:52:30 -0700

    08/10 - removing main function from class @jondoetsch27

--- src/main/kotlin/com/jdd/springboot/kotlin/Application.kt
+++ src/main/kotlin/com/jdd/springboot/kotlin/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Application {
-    fun main(args: Array<String>) {
-        runApplication<Application>(*args)
-    }
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }

2cd68b9559b4dca703458cdddf763b1e894933c2
jondoetsch27 <46655911+jondoetsch27@users.noreply.github.com>
2021-08-11 11:52:17 -0700

    08/11 - service layer updates @jondoetsch27

--- src/main/kotlin/com/jdd/springboot/kotlin/service/impl/PlayerServiceImpl.kt
+++ src/main/kotlin/com/jdd/springboot/kotlin/service/impl/PlayerServiceImpl.kt
@@ -7,6 +7,7 @@ import com.jdd.springboot.kotlin.repository.PlayerRepository
 import com.jdd.springboot.kotlin.service.PlayerService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class PlayerServiceImpl : PlayerService {
@@ -14,12 +15,14 @@ class PlayerServiceImpl : PlayerService {
     @Autowired
     lateinit var playerRepository: PlayerRepository
 
+    @Transactional
     override fun listPlayers(): List<Player> {
         return playerRepository.findAll().toList().ifEmpty {
             throw PlayerNotFoundException()
         }
     }
 
+    @Transactional
     override fun readPlayer(playerName: String): Player {
         return if (playerRepository.findById(playerName).isPresent) {
             playerRepository.findById(playerName).get()
@@ -28,6 +31,7 @@ class PlayerServiceImpl : PlayerService {
         }
     }
 
+    @Transactional
     override fun createPlayer(newPlayer: Player): Player {
         return if (playerRepository.existsById(newPlayer.playerId)) {
             throw DuplicatePlayerException()
@@ -36,6 +40,7 @@ class PlayerServiceImpl : PlayerService {
         }
     }
 
+    @Transactional
     override fun updatePlayer(newPlayer: Player): Player {
         if (playerRepository.existsById(newPlayer.playerId)) {
             playerRepository.deleteById(newPlayer.playerId)
@@ -45,6 +50,7 @@ class PlayerServiceImpl : PlayerService {
         return playerRepository.save(newPlayer)
     }
 
+    @Transactional
     override fun deletePlayer(oldPlayer: Player): Player {
         if (playerRepository.existsById(oldPlayer.playerId)) {
             playerRepository.deleteById(oldPlayer.playerId)

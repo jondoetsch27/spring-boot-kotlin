a31009c9251041a2d603b0d6f450766980df0d0d
jondoetsch27 <46655911+jondoetsch27@users.noreply.github.com>
2021-08-11 14:32:15 -0700

    08/11 - added MockK libraries @jondoetsch27

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
     testImplementation(kotlin("test"))
 }
 
